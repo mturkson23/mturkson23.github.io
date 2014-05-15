619c6e6f9147690583aaf0748e388a96c02491ca
mturkson23 <mturkson23@gmail.com>
2014-05-15 02:50:49 +0000

    initial commit

---
+++ jayrhomi/scripts/controllers/home.ctrl.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('home.ctrl', [])
+  .controller('HomeCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ jayrhomi/scripts/controllers/projects.ctrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('projects.ctrl', [])
+
+.controller('ProjectsCtrl', function($scope) {
+  $scope.nav_model = 'Our Work';
+})
