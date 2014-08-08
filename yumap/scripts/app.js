82838483492da3a301e6804547de4c59052bab2f
mturkson23 <mturkson23@gmail.com>
2014-08-08 16:03:35 +0000

    changed the boring static map image to an interactive google map git add styles/app.css yaygit add styles/app.css

--- yumap/scripts/app.js
+++ yumap/scripts/app.js
@@ -1,21 +1,28 @@
-function geolocate() {
-  var output = document.getElementById("latlng");
-  if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
-  }
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
-
-    output.innerHTML = latitude + ', ' + longitude;
-    var img = new Image();
-    img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=18&size=300x300&markers=color:blue%7Clabel:S%7C"+ latitude +", "+ longitude +"&sensor=true";
-    output.appendChild(img);
-  };
-  function error() {
-    output.innerHTML = "Unable to retrieve your location";
-  };
-  output.innerHTML = "<p>Locating...</p>";
-  navigator.geolocation.getCurrentPosition(success, error);
+var map; 
+function geoLocate() {
+	var output = document.getElementById("latlng");
+	// check if the device has access to the gps functionality. Alert if not
+	if(!navigator.geolocation)
+	{
+		output.innerHTML = "<p>Sorry. Geolocation does not work in your browser :(</p>";
+		return;
+	}
+	// if everything works out well. this would be called
+	function success(position)
+	{
+		var latitude = position.coords.latitude;
+		var longitude = position.coords.longitude;
+		var mapOptions = {
+			zoom: 16,
+			center: new google.maps.LatLng(latitude, longitude)
+		};
+		map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	}
+	//in case there is an error
+	function error()
+	{
+		output.innerHTML = "Unable to retrieve your location";
+	}
+	navigator.geolocation.getCurrentPosition(success, error);
 }
+google.maps.event.addDomListener(window, 'load', geoLocate);
