ed7e94bf672d1cc525addf90f21122dc5cbe1190
mturkson23 <mturkson23@gmail.com>
2014-08-08 16:35:31 +0000

    little spring cleaning

--- yumap/scripts/app.js
+++ yumap/scripts/app.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+// css loading animation !!
+	$('.green').addClass('sd0');
+	$('.red').addClass('sd05');
+	$('.blue').addClass('sd1');
+	$('.yellow').addClass('sd15');
+
 var map; 
 function geoLocate() {
 	var output = document.getElementById("latlng");
@@ -10,13 +17,23 @@ function geoLocate() {
 	// if everything works out well. this would be called
 	function success(position)
 	{
+		// get the location information from the device
 		var latitude = position.coords.latitude;
 		var longitude = position.coords.longitude;
+		
+		// store it in a var so we can use it later
+		var locs = new google.maps.LatLng(latitude, longitude);
 		var mapOptions = {
 			zoom: 16,
-			center: new google.maps.LatLng(latitude, longitude)
+			center: locs
 		};
 		map = new google.maps.Map(document.getElementById('map'), mapOptions);
+		
+		var marker = new google.maps.Marker({
+			position: locs,
+			map: map,
+			title: "I'm Here!"
+		});
 	}
 	//in case there is an error
 	function error()
@@ -26,3 +43,5 @@ function geoLocate() {
 	navigator.geolocation.getCurrentPosition(success, error);
 }
 google.maps.event.addDomListener(window, 'load', geoLocate);
+
+});
